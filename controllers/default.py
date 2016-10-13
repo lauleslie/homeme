1b74b07a9fb9e3017e50061dd352c71fe2dbad01
Josh Sibayan <jbsibaya@ucsc.edu>
2016-10-12 21:11:19 -0700

    Edit posts displaying weird, new users can't view page

--- controllers/default.py
+++ controllers/default.py
@@ -7,6 +7,7 @@
 # - user is required for authentication and authorization
 # - download is for downloading files uploaded in the db (does streaming)
 # -------------------------------------------------------------------------
+import json
 
 def get_user_name_from_email(email):
     """Returns a string corresponding to the user first and last names,
@@ -27,7 +28,13 @@ def index():
     # most recent first, and you need to return that list here.
     # Note that posts is NOT a list of strings in your actual code; it is
     # what you get from a db(...).select(...).
-    posts = ['banana', 'pear', 'eggplant']
+    # posts = ['banana', 'pear', 'eggplant']
+
+    posts = []
+    if auth.user_id is not None:
+        posts = db(db.post.user_email == auth.user.email).select(
+            orderby=~db.post.updated_on
+        )
     return dict(posts=posts)
 
 
@@ -36,7 +43,91 @@ def edit():
     """
     This is the page to create / edit / delete a post.
     """
-    return dict()
+    p = None
+    post_list = []
+    if request.args(0) is None:
+        # If argument zero in URL is empty then it must be a create form
+        form_type = 'create'
+
+        form = SQLFORM.factory(
+            Field('title'),
+            Field('post_items', 'list:string', label='Posts'),
+        )
+
+        # Create form that enables insertion and name database table form refers to
+        # form = SQLFORM(db.post)
+    else:
+        # URL is referencing specific post
+        # Check that post exists and if user is author of post
+        # first() used to get either first element or none instead of iterator
+        query = ((db.post.user_email == auth.user.email) &
+                 (db.post.id == request.args(0)))
+
+        p = db(query).select().first()
+        if p is None:
+            session.flash = T('Not Authorized')
+            redirect(URL('default', 'index'))
+
+        # Update created_on and updated_on date
+        p.created_on = datetime.datetime.utcnow()
+        p.updated_on = datetime.datetime.utcnow()
+        p.update_record()
+
+        # Post now confirmed to be valid
+        # Boolean to check user intent to edit, if not then just view
+        is_edit = (request.vars.edit == 'true')
+        form_type = 'edit' if is_edit else 'view'
+
+        # Extract post content
+        post_list = None
+        try:
+            post_list = json.loads(p.post)
+        except:
+            pass
+        if not isinstance(post_list, list):
+            # When try causes error, execute here
+            # If string in database then
+            if isinstance(p.post, basestring):
+                post_list = [p.post]
+            else:
+                post_list = []
+
+        form = SQLFORM.factory(
+            Field('title'),
+            Field('post_items', 'list:string', default=post_list, label='Posts', writable=is_edit),
+        )
+
+        # form = SQLFORM(db.post, record=p, deletable=is_edit, readonly=not is_edit)
+
+    # Add necessary buttons for each user action
+    button_list = []
+    if form_type == 'edit':
+        button_list.append(A('Cancel', _class='btn btn-warning',
+                             _href=URL('default', 'edit', args=[p.id])))
+    elif form_type == 'create':
+        button_list.append(A('Cancel', _class='btn btn-warning',
+                             _href=URL('default', 'index')))
+    elif form_type == 'view':
+        button_list.append(A('Edit', _class='btn btn-warning',
+                             _href=URL('default', 'edit', args=[p.id], vars=dict(edit='true'))))
+        button_list.append(A('Back', _class='btn btn-primary',
+                             _href=URL('default', 'index')))
+
+    if form.process().accepted:
+        # Post already validated and posted at this point
+        # Update and insert record
+        items = json.dumps(form.vars.post_items)
+        if form_type == 'create':
+            db.post.insert(post_content=form.vars.post_items)
+            session.flash = T('Post created!')
+        else:
+            session.flash = T('Content saved')
+            p.post = items
+            p.update_record()
+        redirect(URL('default', 'index'))
+    elif form.errors:
+        session.flash = T('Get it right this time')
+    return dict(form=form, button_list=button_list, p=p, form_type=form_type, post_list=post_list)
 
 
 def user():
