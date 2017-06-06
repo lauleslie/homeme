00b53f2c6138a0cf516ee82c17709d5db2f49a83
Hanlin Chen <hchen47@ucsc.edu>
2017-06-06 15:57:47 -0700

    source siting, code cleaning part 1

---
+++ models/db.py
@@ -0,0 +1,129 @@
+# -*- coding: utf-8 -*-
+
+# This scaffolding model makes your app work on Google App Engine too
+# File is released under public domain and you can use without limitations
+
+if request.global_settings.web2py_version < "2.14.1":
+    raise HTTP(500, "Requires web2py 2.13.3 or newer")
+
+# if SSL/HTTPS is properly configured and you want all HTTP requests to
+# be redirected to HTTPS, uncomment the line below:
+# request.requires_https()
+
+GOOGLEMAP_KEY="ABQIAAAAT5em2PdsvF3z5onQpCqv0RRbmBRVqcetsYYPAZkdVkJ1U_-f7hQyRsL4C5GpXpg29_Qza9VuE2zX2w"
+
+try:
+    from gluon.contrib.gql import *  # if running on Google App Engine
+except:
+    db = DAL('sqlite://storage.db')  # if not, use SQLite or other DB
+else:
+    db = GQLDB()  # connect to Google BigTable
+    session.connect(request, response, db=db)  # and store sessions there
+
+# app configuration made easy. Look inside private/appconfig.ini
+from gluon.contrib.appconfig import AppConfig
+
+# once in production, remove reload=True to gain full speed
+myconf = AppConfig(reload=True)
+
+if not request.env.web2py_runtime_gae:
+    # if NOT running on Google App Engine use SQLite or other DB
+    db = DAL(myconf.get('db.uri'),
+             pool_size=myconf.get('db.pool_size'),
+             migrate_enabled=myconf.get('db.migrate'),
+             check_reserved=['all'])
+    # I like to keep the session in the db.
+    session.connect(request, response, db=db)
+else:
+    # connect to Google BigTable (optional 'google:datastore://namespace')
+    db = DAL('google:datastore+ndb')
+    # store sessions and tickets there
+    session.connect(request, response, db=db)
+    #
+    # or store session in Memcache, Redis, etc.
+    # from gluon.contrib.memdb import MEMDB
+    # from google.appengine.api.memcache import Client
+    # session.connect(request, response, db = MEMDB(Client()))
+
+# by default give a view/generic.extension to all actions from localhost
+# none otherwise. a pattern can be 'controller/function.extension'
+response.generic_patterns = ['*'] if request.is_local else []
+
+# choose a style for forms
+response.formstyle = myconf.get('forms.formstyle')  # or 'bootstrap3_stacked' or 'bootstrap2' or other
+response.form_label_separator = myconf.get('forms.separator') or ''
+
+# (optional) optimize handling of static files
+# response.optimize_css = 'concat,minify,inline'
+# response.optimize_js = 'concat,minify,inline'
+
+# (optional) static assets folder versioning
+# response.static_version = '0.0.0'
+
+# Here is sample code if you need for
+# - email capabilities
+# - authentication (registration, login, logout, ... )
+# - authorization (role based authorization)
+# - services (xml, csv, json, xmlrpc, jsonrpc, amf, rss)
+# - old style crud actions
+# (more options discussed in gluon/tools.py)
+
+from gluon.tools import Auth, Crud, Service, PluginManager, prettydate
+
+
+# host names must be a list of allowed host names (glob syntax allowed)
+auth = Auth(db, host_names=myconf.get('host.names'))
+service = Service()
+crud, service, plugins = Crud(db), Service(), PluginManager()
+
+
+#defining extra fields for auth_user
+import datetime
+
+auth.settings.extra_fields['auth_user'] = [
+    Field('your_country', label= 'Country'),
+    Field('your_state', label = 'State'),
+    Field('your_county', label = 'Zip Code'),
+    Field('landlord', 'boolean'),
+    Field('tenant', 'boolean' ),
+    Field('about_yourself', 'text'),
+
+]
+
+
+
+# create all tables needed by auth if not custom tables
+auth.define_tables(username=False, signature=False)
+
+
+
+
+# configure email
+mail = auth.settings.mailer
+mail.settings.server = 'logging' if request.is_local else myconf.get('smtp.server')
+mail.settings.sender = myconf.get('smtp.sender')
+mail.settings.login = myconf.get('smtp.login')
+mail.settings.tls = myconf.get('smtp.tls') or False
+mail.settings.ssl = myconf.get('smtp.ssl') or False
+
+# configure auth policy
+auth.settings.registration_requires_verification = False
+auth.settings.registration_requires_approval = False
+auth.settings.reset_password_requires_verification = True
+
+# More API examples for controllers:
+#
+# >>> db.mytable.insert(myfield='value')
+# >>> rows = db(db.mytable.myfield == 'value').select(db.mytable.ALL)
+# >>> for row in rows: print row.id, row.myfield
+
+######################
+# Logging
+import logging, sys
+FORMAT = "%(asctime)s %(levelname)s %(process)s %(thread)s %(funcName)s():%(lineno)d %(message)s"
+logging.basicConfig(stream=sys.stderr)
+logger = logging.getLogger(request.application)
+logger.setLevel(logging.INFO)
+
+# Let's log the request.
+logger.info("====> Request: %r %r %r %r" % (request.env.request_method, request.env.path_info, request.args, request.vars))
---
+++ models/tables.py
@@ -0,0 +1,92 @@
+# Define your tables below (or better in another model file) for example
+#
+# >>> db.define_table('mytable', Field('myfield', 'string'))
+#
+# Fields can be 'string','text','password','integer','double','boolean'
+#       'date','time','datetime','blob','upload', 'reference TABLENAME'
+# There is an implicit 'id integer autoincrement' field
+# Consult manual for more options, validators, etc.
+
+import datetime
+
+
+
+# Create table to be called by SQLFORM
+db.define_table('post',
+                Field('user_email', default=auth.user.email if auth.user_id else None),
+                Field('post_content', 'text'),
+                Field('created_on', 'datetime', default=datetime.datetime.utcnow()),
+                Field('updated_on', 'datetime', update=datetime.datetime.utcnow()),
+                Field('min_budget'),
+                Field('max_budget'),
+                Field('number_of_people'),
+                Field('description'),
+                Field('my_city'),
+                Field('latitude','double',writable=False,  readable = False),
+                Field('longitude','double',writable=False , readable = False)
+                )
+
+                #landlord stuff
+db.define_table('post_landlord',
+                 Field('user_email', default=auth.user.email if auth.user_id else None),
+                 Field('createdon', 'datetime', default=datetime.datetime.utcnow()),
+                 Field('updatedon', 'datetime', update=datetime.datetime.utcnow()),
+                 Field('my_address'),
+                 Field('my_city'),
+                 Field('my_state'),
+                 Field('sq_ft'),
+                 Field('num_bed'),
+                 Field('num_bath'),
+                 Field('washdry', 'boolean'),
+                 Field('furnish', 'boolean'),
+                 Field('pets', 'boolean'),
+                 Field('more_info', 'text'),
+                 Field('picture', 'upload'),
+                 Field('latitude','double',writable=False , readable = False),
+                 Field('longitude','double',writable=False, readable = False)
+                )
+
+
+
+
+# a table to link two people
+db.define_table('link',
+                Field('src','reference auth_user'),
+                Field('target','reference auth_user'),
+                Field('accepted','boolean',default=False))
+
+# a table designed to send an app to a person
+db.define_table('app',
+                Field('src','reference auth_user'),
+                Field('target','reference auth_user'))
+
+# and define some global variables that will make code more compact
+User, Link, ApTab, Post = db.auth_user, db.link, db.app, db.post
+
+#me, accepted people, rejected people
+me, a0, a1 = auth.user_id, request.args(0), request.args(1)
+myfriends = db(Link.src==me)(Link.accepted==True)
+
+myapps = db(ApTab.target==me)
+
+alphabetical = User.first_name|User.last_name
+
+def name_of(user): return '%(first_name)s %(last_name)s' % user
+
+
+# I don't want to display the user email by default in all forms.
+db.post.user_email.readable = db.post.user_email.writable = False
+db.post.id.readable = db.post.id.writable = False
+db.post.post_content.requires = IS_NOT_EMPTY()
+db.post.created_on.readable = db.post.created_on.writable = False
+db.post.updated_on.readable = db.post.updated_on.writable = False
+db.post_landlord.createdon.readable = db.post_landlord.createdon.writable = False
+db.post_landlord.updatedon.readable = db.post_landlord.updatedon.writable = False
+
+db.post.number_of_people.requires = IS_INT_IN_RANGE(0,10)
+db.post.description.requires = IS_NOT_EMPTY()
+#db.post.budget.readable = db.post.budget.writable = False
+#db.post.number_of_people.readable = db.post.number_of_people.writable = False
+#db.post.description.readable = db.post.description.writable = False
+# after defining tables, uncomment below to enable auditing
+# auth.enable_record_versioning(db)
